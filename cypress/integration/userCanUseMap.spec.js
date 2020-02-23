e5e78cb3754aefca58fe1818aa0d2993cc4d5302
piavw <piavonwachenfelt@hotmail.com>
2020-02-23 17:49:37 +0100

    Imports google-maps-react, creates featuretest and fixturefile.

---
+++ cypress/integration/userCanUseMap.spec.js
@@ -0,0 +1,24 @@
+describe('User can use map', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.visit('http://localhost:3001')
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3001',
+      response: 'fixture:gm_map_markers.json',
+      status: 200
+    })
+  })
+  
+  it('successfully see zoom-in and out buttons', () => {
+    cy.get('[title="Zoom in"]').should('exist')
+    cy.get('[title="Zoom out"]').should('exist')
+  })
+
+  it('can interact by zooming-in and out', () => {
+    cy.get('.gm-style > .gmnoprint > .gmnoprint:nth-child(1) > div > .gm-control-active:nth-child(1)').click()
+    cy.get('.gm-style > .gmnoprint > .gmnoprint:nth-child(1) > div > .gm-control-active:nth-child(1)').click()
+    cy.get('.gm-style > .gmnoprint > .gmnoprint > div > .gm-control-active:nth-child(3)').click()
+    cy.get('.gm-style > .gmnoprint > .gmnoprint > div > .gm-control-active:nth-child(3)').click()
+  })
+})
