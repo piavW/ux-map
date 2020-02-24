c7f3200ebf3aa59434058c21c6e60d40ff786895
piavw <piavonwachenfelt@hotmail.com>
2020-02-24 16:07:49 +0100

    Creates dark & light theme, adds button to App.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import Map from './MapComponent'
-import { Header } from 'semantic-ui-react'
+import { Header, Icon } from 'semantic-ui-react'
+import { css, jsx } from '@emotion/core'
+import { useTheme } from 'emotion-theming'
 
-const App = () => {
+const App = ( {isDark, setIsDark} ) => {
+  const theme = useTheme()
+
   return (
-    <>
-    <br/>
+    <div 
+      css={css`
+        padding: 50px 0;
+        background-color: ${theme.background};
+        color: ${theme.text};
+        text-align: center;
+        height: 100vh;
+        transition-duration: 0.2s;
+        transition-property: background-color, color;
+      `}>
+      
+      <br/>
+      <button
+        css={css`
+        border: 2px solid ${theme.buttonBorder}
+        background-color: ${theme.buttonBg}
+        color: ${theme.buttonText}
+        padding: 14px 28px
+        `}
+        onclick={() => {
+          setIsDark(!isDark)
+        }}
+      >
+        Change to {isDark? 'light' : 'dark'} mode <Icon size='tiny' name='adjust'></Icon>
+      </button>
       <center>
         <Header as='h2' id='header' content='UX-Map, find your team-mates'/>
       </center>
       <div className='map-container'>
         <Map />
       </div>
-    </>
+    </div>
   )
 }
 export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState }from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import 'semantic-ui-css/semantic.min.css'
+import { ThemeProvider } from 'emotion-theming'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const lightTheme = {
+  text: '#000',
+  background: '#fff',
+  buttonText: '#fff',
+  buttonTextHover:'#fff',
+  buttonBorder: '#fff',
+  buttonBg: 'rgba(0,0,0,0)',
+  buttonBgHover: 'rgba(0,0,0,1)',
+}
+
+const darkTheme = {
+  text: '#fff',
+  background: '#121212',
+  buttonText: '#fff',
+  buttonTextHover:'#000',
+  buttonBorder: '#fff',
+  buttonBg: 'rgba(255,255,255,0)',
+  buttonBgHover: 'rgba(255,255,255,1)',
+}
+function Root() {
+  const [isDark, setIsDark] = useState(false) 
+  return (
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+    <App isDark={isDark} setIsDark={setIsDark}/>
+    </ThemeProvider>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<Root />, rootElement)
 serviceWorker.unregister()
